080b9d9977b79b6e7375c875ed8ba9de04d43e99
josuebrunel <josuebrunel@gmail.com>
2014-07-21 12:42:58 +0200

    fix #3 logs OK

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,7 +4,17 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-#
-#__all__ = ['views.py']
+#App Logs
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler(app.config['LOG_FOLDER']+'/'+app.config['LOG_FILENAME'], 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info(app.config['LOG_INFO'])
+
+
 from . import views
 from . import api
--- server/config.py
+++ server/config.py
@@ -6,8 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSFR_ENABLED = True
 SECRET_KEY = uuid4()
 
-#TINY URL
+#SHORTENER SERVICE SETTINGS
 TINY_API_URL = 'http://tiny-url.info/api/v1/create'
 TINY_API_KEY = '8A6CIC8081B6BC5G5A07'
 TINY_API_PROVIDER = 'go_ly'
 TINY_API_FORMAT = 'json'
+
+#LOGS SETTINGS
+LOG_FOLDER = 'logs'
+LOG_FILENAME = 'smoothyurl.log'
+LOG_INFO = 'Smoothy url startup'
